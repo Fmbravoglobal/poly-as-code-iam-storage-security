4c4117f2671261b666dfb3f1d79363cc6404de70
Oluwafemi Alabi Okunlola <ooluwafemiokunlola308@gmail.com>
2026-03-05 22:42:44 -0600

    feat(terraform): add outputs for IAM role and secure S3 bucket resources

---
+++ terraform/output.tf
@@ -0,0 +1,19 @@
+Aoutput "bucket_name" {
+  description = "Name of the secure S3 bucket"
+  value       = aws_s3_bucket.secure_bucket.bucket
+}
+
+output "bucket_arn" {
+  description = "ARN of the secure S3 bucket"
+  value       = aws_s3_bucket.secure_bucket.arn
+}
+
+output "iam_role_name" {
+  description = "IAM role for secure application access"
+  value       = aws_iam_role.app_role.name
+}
+
+output "policy_arn" {
+  description = "ARN of the least privilege IAM policy"
+  value       = aws_iam_policy.s3_readonly_policy.arn
+}
